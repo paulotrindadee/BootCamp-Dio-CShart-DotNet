4af02f146ab3146435fa7a13959273f72e4f6d95
Paulo Trindade <paulotrindade90@outlook.com>
2024-10-25 22:11:24 -0300

    versão 1.1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,36 @@
-﻿using PrimeiroProjeto.Models; // Utilizar para que seja um caminho, só assim será reconhecida a class 
+﻿using System.Threading.Tasks.Dataflow;
+using PrimeiroProjeto.Models; // Utilizar para que seja um caminho, só assim será reconhecida a class 
 
-Pessoa pessoa1 = new Pessoa();  //New Pessoa é a construção do objeto e pode ser mudada a caracteristicas da pessoa.
+// Pessoa pessoa1 = new Pessoa();  //New Pessoa é a construção do objeto e pode ser mudada a caracteristicas da pessoa.
 
-//instancia
-pessoa1.Nome = "Paulo";
-pessoa1.Idade = 34;
+
+// string apresentacao = "Olá, seja bem vindo";
+
+// int quantidade = 10;
+// double altura = 1.79;
+// decimal preco = 3.70M;
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);   
+// Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+// Console.WriteLine("Valor da variável preço: " + preco);
+// Console.WriteLine("Valor da variável condição: " + condicao);
+
+// instancia
+
+//pessoa1.Nome = "Paulo";
+//pessoa1.Idade = 34;
 
 //metódo
-pessoa1.Apresentar();
+//pessoa1.Apresentar();
+
+
+
+
+
+DateTime dataAtual = DateTime.Now;
+
+Console.WriteLine(dataAtual);
 
 
